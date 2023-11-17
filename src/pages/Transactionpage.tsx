3663edf3ef82293358ec2a14dfef6d796476169a
Alfred Osang <osangalfred@gmail.com>
2023-11-17 04:21:03 -0800

    modal state update

--- src/pages/Transactionpage.tsx
+++ src/pages/Transactionpage.tsx
@@ -3,7 +3,7 @@ import { Fragment, useState } from 'react'
 
 const Transactionpage = () => {
 
-    let [isOpen, setIsOpen] = useState(true)
+    let [isOpen, setIsOpen] = useState(false)
 
     function closeModal() {
         setIsOpen(false)
